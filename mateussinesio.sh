1640374f961320900025a29115120ea220447c92
Mateus Sinésio <mateussinesio@gmail.com>
2024-12-25 22:44:43 -0300

    the repository folder in the cp command has been renamed

--- mateussinesio.sh
+++ mateussinesio.sh
@@ -4,7 +4,7 @@ sudo apt install -y xserver-xorg-core xinit x11-utils x11-xserver-utils
 sudo apt install -y pipewire pipewire-alsa pipewire-jack pipewire-audio wireplumber pipewire-pulse
 sudo apt install -y bspwm sxhkd polybar feh rofi alacritty firefox-esr
 
-cp -r ~/arquivoslinux/.config/ ~/
+cp -r ~/arquivos-linux/.config/ ~/
 
 chmod +x ~/.config/bspwm/bspwmrc
 chmod +x ~/.config/sxhkd/sxhkdrc
